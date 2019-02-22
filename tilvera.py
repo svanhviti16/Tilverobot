7a97d7cc54c74d6b5a835ee1a159a41b89a5f3c5
Svanhvit <svanhviti16@ru.is>
2019-02-22 23:24:58 +0000

    production

--- tilvera.py
+++ tilvera.py
@@ -1,17 +1,23 @@
-
 from random import choice
 import sys, tweepy, requests
+from credentials import *
 
-
-# Accepts .txt files as N parameters
+# Accepts .txt files as 3 parameters
 # Outputs random words from the files in a sentence
 with open(sys.argv[1], encoding="utf-8") as lo_hk, open(sys.argv[2], encoding="utf-8") as no_hk, open(sys.argv[3], encoding="utf-8") as no_kk:
     lo_list = [word.strip() for word in lo_hk]
     no_hk_list = [word.strip() for word in no_hk]
     no_kk_list = [word.strip() for word in no_kk]
 
-print(f'Tilvera okkar er {choice(lo_list)} {choice(no_hk_list)}. Við erum {choice(no_kk_list)} og {choice(no_hk_list)} okkar er {choice(no_hk_list)}.')
-#Tilvera okkar er hálflofað tuskuílag. Við erum hitahlaup og hljóðfræðirit okkar er hallklæmt.
-#Tilvera okkar er handfornskt músamálverk. Við erum óhrím og akurpúlt okkar er varfeilmult.
-#Tilvera okkar er horfautt smit. Við erum slefudrættir og fransvín okkar er goslíki.
-#Tilvera okkar er hráeygt þæginöl. Við erum dyrabitar og ullarmerki okkar er fjölskylduverkalyf.
+def tweet_the_thing(tweet):
+    tw_auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
+    tw_auth.set_access_token(twitter_access_token, twitter_access_token_secret)
+    tw_api = tweepy.API(tw_auth)
+    tw_api.update_status(tweet)
+
+    print(tweet)
+
+def getTweet():
+    return f'Tilvera okkar er {choice(lo_list)} {choice(no_hk_list)}. Við erum {choice(no_kk_list)} og {choice(no_hk_list)} okkar er {choice(no_hk_list)}.'
+
+tweet_the_thing(getTweet())
